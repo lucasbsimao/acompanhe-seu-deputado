eec9f78eca0e2a344420748aa8cacf0d1af19936
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2026-06-27 19:32:18 -0300

    feat(db): 39 add id column to tse_donations table

--- app/src/shared/db/migrations/011_create_tse_donations_table.ts
+++ app/src/shared/db/migrations/011_create_tse_donations_table.ts
@@ -5,6 +5,7 @@ import type { Migration } from '../migrations';
 export const migration011: Migration = {
   version: '011_create_tse_donations_table',
   sql: `CREATE TABLE IF NOT EXISTS tse_donations (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   donor_cpf TEXT NOT NULL,
   recipient_cpf TEXT NOT NULL,
   ano_eleicao INTEGER NOT NULL,
